fc216ecc7ed1e3ffaeebe984e0f95e21998fbbd5
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-03-02 23:32:14 +0300

    feat: add calculateTip function and reset button functionality

--- TipCalculatorApp/script.js
+++ TipCalculatorApp/script.js
@@ -1,13 +1,34 @@
-const billInput=document.querySelector("#bill")
-const tipBtns=document.querySelectorAll(".tip-btn")
-const customTipInput=document.querySelector(".custom-tip")
-const errorMsg=document.querySelector("#people-error")
-const peopleInput=document.querySelector("#people")
-const tipAmount=document.querySelector("#tip-amount-person")
-const totalPerson=document.querySelector("#total-person")
+const billInput = document.querySelector("#bill")
+const tipBtns = document.querySelectorAll(".tip-btn")
+const customTipInput = document.querySelector(".custom-tip")
+const peopleInput = document.querySelector("#people")
+const errorMsg = document.querySelector("#people-error")
+const tipAmountDisplay = document.querySelector("#tip-amount-person")
+const totalPersonDisplay = document.querySelector("#total-person")
+const resetBtn = document.querySelector(".reset-btn")
 
-function calculateTip(){
+let bilValue = 0.0
+let tipPercent = 0.0
+let peopleValue = 1
+
+function calculateTip() {
+    if (peopleValue >= 1) {
+        let totalTip = bilValue * tipPercent
+        let tipPerPerson = totalTip / peopleValue
+        let totalPerPerson = (bilValue + totalTip) / peopleValue
+
+        tipAmountDisplay.textContent = `$${tipPerPerson.toFixed(2)}`
+        totalPersonDisplay.textContent = `$${totalPerPerson.toFixed(2)}`
+    }
 }
 
+resetBtn.addEventListener("click",()=>{
+    billInput.value=""
+    peopleInput.value=""
+    customTipInput.value=""
+    tipAmountDisplay.textContent="$0.00"
+    totalPersonDisplay.textContent="$0.00"
 
-peopleInput.addEventListener("input",calculateTip)
+})
+
+peopleInput.addEventListener("input", calculateTip)

41af0c89aa0e6160691ca9bef0923fae9a994bad
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-03-11 23:12:49 +0300

    feat(age calculator app): add basic age calculation logic

--- AgeCalculatorApp/script.js
+++ AgeCalculatorApp/script.js
@@ -1,17 +1,38 @@
 const form = document.querySelector("form")
+
 const dayInput = document.querySelector("#day")
-const daysSpan = document.querySelector("#days")
 const monthInput = document.querySelector("#month")
-const monthsSpan = document.querySelector("#months")
 const yearInput = document.querySelector("#year")
-const yearSpan = document.querySelector("#years")
-const inputGroup = document.querySelector(".input-group")
+
+const daysSpan = document.querySelector("#days")
+const monthsSpan = document.querySelector("#months")
+const yearsSpan = document.querySelector("#years")
+
 function calculateAge(e) {
     e.preventDefault()
     let day = parseInt(dayInput.value);
     let month = parseInt(monthInput.value);
     let year = parseInt(yearInput.value);
 
-    
+    const today = new Date()
+    const birthDate = new Date(year, month - 1, day)
+
+    let years = today.getFullYear() - birthDate.getFullYear()
+    let months = today.getMonth() - birthDate.getMonth()
+    let days = today.getDate() - birthDate.getDate()
+
+    if (days < 0) {
+        months--
+        const lastMonth = new Date(today.getFullYear(), today.getMonth(), 0)
+        days += lastMonth.getDate()
+    }
+    if (months < 0) {
+        years--
+        months += 12
+    }
+    yearsSpan.textContent = years
+    monthsSpan.textContent = months
+    daysSpan.textContent = days
+
 }
 form.addEventListener("submit", calculateAge)

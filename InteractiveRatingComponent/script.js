ac488f72afbeb0740843f797203ae3dfb26e7d5b
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-03-16 06:09:01 +0300

    a11y: improve accessibility of rating component

--- InteractiveRatingComponent/script.js
+++ InteractiveRatingComponent/script.js
@@ -10,8 +10,13 @@ const ratingState = {
 
 ratingBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        ratingBtns.forEach(b => b.classList.remove("active"))
+        ratingBtns.forEach(b => {
+            b.classList.remove("active")
+            b.setAttribute("aria-pressed", "false")
+        })
+
         btn.classList.add("active")
+        btn.setAttribute("aria-pressed", "true")
         ratingState.selectedRate = btn.value
         ratingValue.textContent = btn.value
     })
@@ -21,6 +26,9 @@ form.addEventListener("submit", (e) => {
     e.preventDefault()
     if (!(ratingState.selectedRate)) return
     rateCard.classList.add("hidden")
+    rateCard.setAttribute("aria-hidden", "true")
+
     thankyouCard.classList.remove("hidden")
+    thankyouCard.setAttribute("aria-hidden", "false")
 })
 

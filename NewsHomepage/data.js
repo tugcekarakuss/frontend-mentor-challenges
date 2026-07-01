c90af173b0984361096822afd7700fced7513d4a
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-07-01 05:04:00 +0300

    fix: import image assets correctly in data file

---
+++ NewsHomepage/data.js
@@ -0,0 +1,65 @@
+export const navigations=[
+    {
+        name:"Home",
+        href:"#"
+    },
+    {
+        name:"New",
+        href:"#"
+    },
+    {
+        name:"Popular",
+        href:"#"
+    },
+    {
+        name:"Trending",
+        href:"#"
+    },
+    {
+        name:"Categories",
+        href:"#"
+    }
+]
+export const images = {
+    logo: "./src/assets/images/logo.svg",
+    iconMenu: "./src/assets/images/icon-menu.svg",
+    iconMenuClose: "./src/assets/images/icon-menu-close.svg",
+    imageWeb3Desktop:"./src/assets/images/image-web-3-desktop.jpg",
+    imageWeb3Mobile:"./src/assets/images/image-web-3-mobile.jpg"
+};
+
+export const articles = [
+    {
+        title:"Hydrogen VS Electric Cars",
+        description:"Will hydrogen-fueled cars ever catch up to EVs?"
+    },
+    {
+        title:"The Downsides of AI Artistry",
+        description:"What are the possible adverse effects of on-demand AI image generation?"
+    },
+    {
+        title:"Is VC Funding Drying Up?",
+        description:"Private funding by VC firms is down 50% YOY. We take a look at what that means."
+    }
+];
+
+export const news_list = [
+    {
+        id: 1,
+        title: "Reviving Retro PCs",
+        description: "What happens when old PCs are given modern upgrades?",
+        image:"./src/assets/images/image-retro-pcs.jpg"
+    },
+    {
+        id: 2,
+        title: "Top 10 Laptops of 2022",
+        description: "Our best picks for various needs and budgets.",
+        image:"./src/assets/images/image-top-laptops.jpg"
+    },
+    {
+        id: 3,
+        title: "The Growth of Gaming",
+        description: "How the pandemic has sparked fresh opportunities.",
+        image:"./src/assets/images/image-gaming-growth.jpg"
+    }
+];

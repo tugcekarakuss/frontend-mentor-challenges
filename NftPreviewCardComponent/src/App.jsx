98aca4d99ebb803cf100900df3fd55569790ef9b
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-07-28 00:03:19 +0300

    feat: initialize NFT preview card project with React, Tailwind, components, and assets

---
+++ NftPreviewCardComponent/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Card from "./components/Card.jsx"
+function App() {
+  return (
+    <>
+      <section className='bg-main-bg min-h-screen flex justify-center items-center font-outfit'>
+        <Card />
+      </section>
+    </>
+  )
+}
+
+export default App

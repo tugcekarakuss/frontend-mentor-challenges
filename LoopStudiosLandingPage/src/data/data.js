f81bac9c149eee04058d80b7a973c0fa044a51cb
Tuğçe Karakuş <karakustugce1453@gmail.com>
2026-08-02 22:35:23 +0300

    chore: initialize project structure with React, Tailwind and assets setup

---
+++ LoopStudiosLandingPage/src/data/data.js
@@ -0,0 +1,99 @@
+import deepEarthDesktop from "../assets/images/creations/desktop/image-deep-earth.jpg";
+import deepEarthMobile from "../assets/images/creations/mobile/image-deep-earth.jpg";
+import nightArcadeDesktop from "../assets/images/creations/desktop/image-night-arcade.jpg";
+import nightArcadeMobile from "../assets/images/creations/mobile/image-night-arcade.jpg";
+import soccerTeamDesktop from "../assets/images/creations/desktop/image-soccer-team.jpg";
+import soccerTeamMobile from "../assets/images/creations/mobile/image-soccer-team.jpg";
+import theGridDesktop from "../assets/images/creations/desktop/image-grid.jpg";
+import theGridMobile from "../assets/images/creations/mobile/image-grid.jpg";
+import fromUpDesktop from "../assets/images/creations/desktop/image-from-above.jpg";
+import fromUpMobile from "../assets/images/creations/mobile/image-from-above.jpg";
+import pocketBorealisDesktop from "../assets/images/creations/desktop/image-pocket-borealis.jpg";
+import pocketBorealisMobile from "../assets/images/creations/mobile/image-pocket-borealis.jpg";
+import curiosityDesktop from "../assets/images/creations/desktop/image-curiosity.jpg";
+import curiosityMobile from "../assets/images/creations/mobile/image-curiosity.jpg";
+import fisheyeDesktop from "../assets/images/creations/desktop/image-fisheye.jpg";
+import fisheyeMobile from "../assets/images/creations/mobile/image-fisheye.jpg";
+
+export const navLinks = [
+    { id: 1, label: "About", href: "#about" },
+    { id: 2, label: "Careers", href: "#careers" },
+    { id: 3, label: "Events", href: "#events" },
+    { id: 4, label: "Products", href: "#products" },
+    { id: 5, label: "Support", href: "#support" },
+];
+
+export const creations = [
+    {
+        id: 1,
+        title: "Deep earth",
+        href: "#",
+        image: {
+            desktop: deepEarthDesktop,
+            mobile: deepEarthMobile
+        }
+    },
+    {
+        id: 2,
+        title: "Night arcade",
+        href: "#",
+        image: {
+            desktop: nightArcadeDesktop,
+            mobile: nightArcadeMobile
+        }
+    },
+    {
+        id: 3,
+        title: "Soccer team VR",
+        href: "#",
+        image: {
+            desktop: soccerTeamDesktop,
+            mobile: soccerTeamMobile
+        }
+    },
+    {
+        id: 4,
+        title: "The grid",
+        href: "#",
+         image: {
+            desktop: theGridDesktop,
+            mobile: theGridMobile
+        }
+    },
+    {
+        id: 5,
+        title: "From up above VR",
+        href: "#",
+        image: {
+            desktop: fromUpDesktop,
+            mobile: fromUpMobile
+        }
+    },
+    {
+        id: 6,
+        title: "Pocket borealis",
+        href: "#",
+        image: {
+            desktop: pocketBorealisDesktop,
+            mobile: pocketBorealisMobile
+        }
+    },
+    {
+        id: 7,
+        title: "The curiosity",
+        href: "#",
+        image: {
+            desktop: curiosityDesktop,
+            mobile: curiosityMobile
+        }
+    },
+    {
+        id: 8,
+        title: "Make it fisheye",
+        href: "#",
+        image: {
+            desktop: fisheyeDesktop,
+            mobile: fisheyeMobile
+        }
+    },
+]
